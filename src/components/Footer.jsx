1005f6dbe54f347db07e9dc15fcf258f8e626fa7
Fernando Saito <fernandosaito@hotmail.com.br>
2022-10-21 18:14:35 -0400

    atualizacao de alguns textos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => (
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make the payments easy, reliable and secure.
+          Impressões que preservam sua marca.
         </p>
       </div>
 
@@ -41,7 +41,8 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+      2022 | Todos os direitos reservados. Conservare Gráfica ® | CNPJ: 22.367.488/0001-22
+
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
